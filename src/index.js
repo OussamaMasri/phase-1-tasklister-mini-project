0a7cce02a4dca5ded0e05795583fc9e5305384ae
Oussama Masri <oussamamasri16120@gmail.com>
2022-08-16 20:26:27 +0300

    Assignment Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form=document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+      console.log(e.target.new-task-description.value)
+      handleTask(e.target.new-task-description.value)
+      e.preventDefault();
+      form.reset()
+  })
+  
+  function handleTask(work){
+      const li=document.createElement('li');
+      li.innerText=`${work}  `;
+      const item=document.querySelector('#tasks')
+      item.appendChild(li)
+      const btn=document.createElement('button');
+      btn.setAttribute('id','#btn')
+      btn.innerHTML='X'
+      li.appendChild(btn)
+      btn.addEventListener('click',(e)=>{e.target.parentNode.remove()})
+  }
+  
+
 });
