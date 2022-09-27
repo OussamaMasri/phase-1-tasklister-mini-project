d034a24d97e387ae0d8f988f19fc65c1a7a5840d
Oussama Masri <67838526+OussamaMasri@users.noreply.github.com>
2022-09-27 22:05:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let form=document.querySelector('form')
-  form.addEventListener('submit',(e)=>{
-      console.log(e.target.new-task-description.value)
-      handleTask(e.target.new-task-description.value)
-      e.preventDefault();
-      form.reset()
+  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    console.log(e.target.new_task_description.value)
+    buildDescription(e.target.new_task_description.value) //returns user input
+    form.reset()
   })
-  
-  function handleTask(work){
-      const li=document.createElement('li');
-      li.innerText=`${work}  `;
-      const item=document.querySelector('#tasks')
-      item.appendChild(li)
-      const btn=document.createElement('button');
-      btn.setAttribute('id','#btn')
-      btn.innerHTML='X'
-      li.appendChild(btn)
-      btn.addEventListener('click',(e)=>{e.target.parentNode.remove()})
-  }
-  
-
 });
+
+function buildDescription(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  let color = document.querySelector('#color')
+  li.setAttribute('style', 'color:' + (color.value).toString())
+  li.className = (color.value)
+  if(li.className === "red") {
+    document.querySelector('#red_list').appendChild(li)
+  }else if(li.className === "yellow"){
+    document.querySelector('#yellow_list').appendChild(li)
+  }else if(li.className === "green"){
+    document.querySelector('#green_list').appendChild(li)
+  }
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
